037210e8060450d5b0424a522cfad8334a0bd080
Andrew Shestihin <shestihinandrey@gmail.com>
2023-02-11 00:28:37 +0300

    update

---
+++ variables.tf
@@ -0,0 +1,24 @@
+
+variable "token" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = ""
+}
+
+variable "folder_id" {
+  default = ""
+}
+
+variable "bucket_name" {
+  default = "shestihin"
+}
+
+variable "zone" {
+  default = "ru-central1-a"
+}
+
+variable "image_id" {
+  default = "fd827b91d99psvq5fjit"
+}
